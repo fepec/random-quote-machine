58e8b57b542bf1ec07602f27bfcf50cf22bd3028
fepec <felipe.estrada.prada@gmail.com>
2023-10-20 18:32:13 -0500

    Static version w/ basic styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
+import "./App.css";
 
-import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareXTwitter } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
+      <QuoteBox quote={QUOTE} />
     </div>
   );
 }
 
+function QuoteBox({ quote }) {
+  return (
+    <div id="quote-box">
+      <QuoteText text={quote.content} />
+      <QuoteAuthor author={quote.author} />
+      <ButtonBar />
+    </div>
+  );
+}
 
+function QuoteText({ text }) {
+  return <p id="text">{text}</p>;
+}
+
+function QuoteAuthor({ author }) {
+  return <p id="author">- {author}</p>;
+}
+
+function ButtonBar() {
+  return (
+    <div className="button-bar">
+      <TweetQuote />
+      <NewQuote />
+    </div>
+  );
+}
+
+function TweetQuote() {
+  return (
+    <div>
+      <a id="tweet-quote">
+        <FontAwesomeIcon icon={faSquareXTwitter} />
+      </a>
+    </div>
+  );
+}
+
+function NewQuote() {
+  return (
+    <>
+      <button id="new-quote">New Quote</button>
+    </>
+  );
+}
 
 export default App;
+
+const QUOTE = {
+  _id: "RmeADLR8F3X2",
+  content:
+    "How wonderful it is that nobody need wait a single moment before starting to improve the world.",
+  author: "Anne Frank",
+  tags: ["Famous Quotes"],
+  authorSlug: "anne-frank",
+  length: 95,
+  dateAdded: "2020-03-27",
+  dateModified: "2023-04-14",
+};
