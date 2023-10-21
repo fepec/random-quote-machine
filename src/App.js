173059294109b3dc074bed49e698da6937cbf650
fepec <felipe.estrada.prada@gmail.com>
2023-10-20 20:24:44 -0500

    complete static styling

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import "./App.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareXTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons"
+import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div>
-      <QuoteBox quote={QUOTE} />
+    <div className="container">
+      <div className="row">
+        <div className="col"></div>
+        <div className="col-md-6 col-sm-10">
+          <QuoteBox quote={QUOTE} />
+        </div>
+        <div className="col"></div>
+      </div>
     </div>
   );
 }
 
 function QuoteBox({ quote }) {
   return (
-    <div id="quote-box">
+    <div id="quote-box" className="container vertical-center bg-white text-primary px-3 py-2 rounded">
       <QuoteText text={quote.content} />
       <QuoteAuthor author={quote.author} />
       <ButtonBar />
@@ -24,32 +30,40 @@ function QuoteBox({ quote }) {
 
 function QuoteText({ text }) {
   return (
-    <div>
-      <FontAwesomeIcon icon={faQuoteLeft} />
-      <span id="text"> {text} </span>
-      <FontAwesomeIcon icon={faQuoteRight} />
+    <div className="row ">
+      <span>
+        <FontAwesomeIcon icon={faQuoteLeft} /> <span id="text"> {text} </span>{" "}
+        <FontAwesomeIcon icon={faQuoteRight} />
+      </span>
     </div>
   );
 }
 
 function QuoteAuthor({ author }) {
-  return <p id="author">- {author}</p>;
+  return (
+    <div className="row text-end pb-3">
+      <span id="author">- {author}</span>
+    </div>
+  );
 }
 
 function ButtonBar() {
   return (
-    <div className="button-bar">
-      <TweetQuote />
-      <NewQuote />
+    <div id="button-bar" className="row row-cols-3 align-items-center justify-content-evenly">
+      
+        <TweetQuote /> 
+        <div className="col"></div>
+        <NewQuote />
+      
     </div>
   );
 }
 
 function TweetQuote() {
   return (
-    <div>
+    <div className="col">
       <a id="tweet-quote">
-        <FontAwesomeIcon icon={faSquareXTwitter} />
+        <FontAwesomeIcon icon={faSquareXTwitter} size="xl" />
       </a>
     </div>
   );
@@ -57,9 +71,9 @@ function TweetQuote() {
 
 function NewQuote() {
   return (
-    <>
-      <button id="new-quote">New Quote</button>
-    </>
+    <div className="col d-flex justify-content-end">
+      <button id="new-quote" className="bg-primary text-light border-0 rounded">New Quote</button>
+    </div>
   );
 }
 
